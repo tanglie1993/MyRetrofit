7f8b48647a09b6782fdf1c2cc39bdc89e74383bd
tanglie1993 <tanglie23@163.com>
2018-06-18 14:26:48 +0800

    success

--- src/main/retrofit/Response.java
+++ src/main/retrofit/Response.java
@@ -1,7 +1,12 @@
 package main.retrofit;
 
 
+import okhttp3.Headers;
+import okhttp3.Protocol;
+import okhttp3.Request;
 import okhttp3.ResponseBody;
+
+import static org.mockito.internal.util.Checks.checkNotNull;
 
 
 /** An HTTP response. */
@@ -35,5 +40,30 @@ public class Response<T> {
 
     public okhttp3.Response raw() {
         return rawResponse;
+    }
+
+    public static <T> Response<T> success(T body) {
+        return success(body, new okhttp3.Response.Builder() //
+                .code(200)
+                .message("OK")
+                .protocol(Protocol.HTTP_1_1)
+                .request(new Request.Builder().url("http://localhost/").build())
+                .build());
+    }
+
+    public static <T> Response<T> success(T body, okhttp3.Response rawResponse) {
+        checkNotNull(rawResponse, "rawResponse == null");
+        if (!rawResponse.isSuccessful()) {
+            throw new IllegalArgumentException("rawResponse must be successful response");
+        }
+        return new Response<>(rawResponse, body, null);
+    }
+
+    public String message() {
+        return rawResponse.message();
+    }
+
+    public Headers headers() {
+        return rawResponse.headers();
     }
 }
